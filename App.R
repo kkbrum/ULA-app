d507f622e8cd0a95c4d6295310d6006eae36c7ce
jcseyhun <38105014+jcseyhun@users.noreply.github.com>
2018-04-15 20:55:44 -0400

    Add files via upload
    Added reactivity to the "summary" tab based on what information was entered in the "my info" tab.

--- App.R
+++ App.R
@@ -2,56 +2,42 @@ library(shiny)
 
 ui <- fluidPage(
   
-  tags$head(
-    tags$style(
-      HTML(".shiny-notification {
-           position:fixed;
-           top: calc(30%);
-           left: calc(50%);
-           width: calc(20%);
-           }",
-           
-           "#loginbox {
-            background-color: #99ccff;
-            padding-left: 20px;
-            padding-bottom: 20px;
-            padding-top: 10px;
-            }
-           "
-      )
-    )
-  ),
-  
   titlePanel("Student View"), 
   
-  mainPanel(width=12,
-            tabsetPanel(type = "tabs",
-                        tabPanel("My Info", 
-                                 textInput("netid", "NetID (New Users Only)", "", width="60%"),
-                                 textInput("new_pin", "Create a 4-digit pin for future login", "", width="60%"),
-                                 textInput("first_name", "First Name", "", width="60%"),
-                                 textInput("last_name", "Last Name", "", width="60%"),
-                                 selectInput("year", "Class Year", 
-                                             c(2018, 2019, 2020, 2021), width="60%"),
-                                 textInput("major", "Major", "", width="60%"),
-                                 textAreaInput("why", 
-                                               "Why do you want to serve as a ULA?", 
-                                               "", width="60%", height="60%"),
-                                 actionButton("submit1", "Submit")
-                        ),
-                        tabPanel("Course Preferences",  tableOutput("courses")),
-                        tabPanel("Summary", actionButton("submit", "Submit"))
-            )
-  ),
-  
-  absolutePanel(id="loginbox", top="15%", right="8%", width="20%", draggable=TRUE,
-                textInput("username", "Username", "", width="90%"),
-                textInput("pin", "4-digit Pin", "", width="90%"),
-                actionButton("login", "Log in")
+  fluidRow(
+    column(width=7, 
+           mainPanel(width=12,
+                     tabsetPanel(type = "tabs",
+                                 tabPanel("My Info",
+                                          br(),
+                                          textInput("netid", "NetID (New Users Only)", "", width="60%"),
+                                          textInput("new_pin", "Create a 4-digit pin for future login", "", width="60%"),
+                                          textInput("first_name", "First Name", "", width="60%"),
+                                          textInput("last_name", "Last Name", "", width="60%"),
+                                          selectInput("year", "Class Year", 
+                                                      c("", 2018, 2019, 2020, 2021), selected="", width="60%"),
+                                          textInput("major", "Major", "", width="60%"),
+                                          textAreaInput("why", 
+                                                        "Why do you want to serve as a ULA?", 
+                                                        "", width="60%", height="60%"),
+                                          actionButton("submit1", "Submit")
+                                 ),
+                                 tabPanel("Course Preferences",  tableOutput("courses")),
+                                 tabPanel("Summary", br(), htmlOutput("summarytext"), br(), actionButton("submit", "Submit"))
+                     )
+           )
+    ),
+    
+    column(width=5, 
+           sidebarPanel(width=12,
+                        textInput("username", "Username", ""),
+                        textInput("pin", "4-digit Pin", "")
+           )
+    )
   )
 )
 
-server <- function(session, input, output) {
+server <- function(input, output) {
   
   observeEvent(input$submit1, {
     write.csv(as.data.frame(cbind("netid"=input$netid, 
@@ -65,26 +51,37 @@ server <- function(session, input, output) {
     paste0(input$netid, "_", input$new_pin, ".csv")
     )
   })
-  observeEvent(input$submit1, {
-    showNotification("Application Successful!", duration=5, type="message")
+  output$courses <- renderTable(read.csv("courses.csv")
+  )
+  
+  output$summarytext <- renderUI({
+    
+    
+    condition1 <- c(input$netid == "", input$new_pin == "", input$first_name == "",
+                    input$last_name == "", input$year == "", input$major == "",
+                    input$why == "")
+    
+    if(all(condition1)) { #All entries are blank
+      expr = HTML("You haven't selected anything yet, silly!") 
+      
+    } else if(!any(condition1)) { #No entries are blank
+      expr = HTML("Super duper! You've filled in all of the fields. You are ready to submit!") 
+      
+    } else {
+      text <- character(7)
+      text[1] <- paste("You have input your netid as", input$netid)
+      text[2] <- "You have input a pin"
+      text[3] <- paste("You have entered your first name as", input$first_name)
+      text[4] <- paste("You have entered your last name as", input$last_name)
+      text[5] <- paste("You have input your class year as", input$year)
+      text[6] <- paste("You have input your major as", input$major)
+      text[7] <- paste("You have input why you want to be a ULA")
+      expr = HTML(paste(text[which(c(input$netid, input$new_pin, input$first_name, input$last_name, 
+                                     input$year, input$major, input$why) != "")], collapse = "<br/>"))
+    }
   })
   
-  output$courses <- renderTable(read.csv("courses.csv"))
-  
-  observeEvent(input$login, {
-    tryCatch({
-      mydata <- read.csv(paste0(input$username, "_", input$pin, ".csv"), header=TRUE)
-      updateTextInput(session, 'netid', value = mydata$netid)
-      updateTextInput(session, 'new_pin', value = mydata$new_pin)
-      updateTextInput(session, 'first_name', value = mydata$first_name)
-      updateTextInput(session, 'last_name', value = mydata$last_name)
-      updateSelectInput(session, 'year', selected = mydata$year)
-      updateTextInput(session, 'major', value = mydata$major)
-      updateTextAreaInput(session, 'why', value = mydata$why)
-    }, error= function(e) {showNotification('User and pin not found', duration=5, type="error")})
-  })
 }
 
 
 shinyApp(ui, server)
-
