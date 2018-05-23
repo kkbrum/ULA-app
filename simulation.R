457f5b779e23bf9b4b8dc3a9b105834707c8dc71
Maria Gargiulo <mariagargiulo.017@gmail.com>
2018-05-22 21:18:45 -0400

    additions to simulation notes

--- simulation.R
+++ simulation.R
@@ -4,6 +4,7 @@
 # variable is set correctly to accommodate for this
 library(matchingMarkets)
 
+# -----------------------------------------------------------------------------
 # Toy version of college admission (hospital/residents) problem
 # Using language relevant to the ULA problem
 
@@ -18,3 +19,22 @@ s.prefs <- matrix(c(1,NA, 1,2, 1,NA, 2,1, 1,2, 1,2, 1,2), 2, 7)
 # one row per student
 c.prefs <- matrix(c(1,2,3,4,5,6,7, 1,2,3,4,5,NA,NA), 7, 2)
 m2 <- hri(s.prefs=s.prefs, c.prefs=c.prefs, nSlots=c(3,3))
+plot(m2, energy=TRUE)
+
+# 100 students, 100 colleges with 1 slot each
+res <- hri(nStudents=200, nColleges=200, seed=64)
+plot(res)
+plot(res, energy=TRUE)
+
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# ULA problem data structures
+
+# dim(s.prefs) <- nrow=nclasses, ncol=nstudents
+# dim(c.prefs) <- nrow=nstudents, ncol=nclasses
+# always need to pad with NA's to ensure proper matrix dims
+# need to map students and classes to numbers
+
+# -----------------------------------------------------------------------------
+
