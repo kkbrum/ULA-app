ec111a47e54d6501ddb54ddb5a20d5480b2f7a81
Maria Gargiulo <mariagargiulo.017@gmail.com>
2018-05-22 21:04:00 -0400

    New simulation notes

---
+++ simulation.R
@@ -0,0 +1,20 @@
+# install.packages('matchingMarkets')
+# Be careful with `rJava` -- make sure that the same version of R and Java are
+# installed on the machine (ex. 64 bit R with 64 bit Java) and make sure the path
+# variable is set correctly to accommodate for this
+library(matchingMarkets)
+
+# Toy version of college admission (hospital/residents) problem
+# Using language relevant to the ULA problem
+
+# 7 students, 2 professors with 3 slots each, random preferences
+m1 <- hri(nStudents=7, nSlots=c(3, 3), seed=64)
+
+# 7 students, 2 professors with 3 slots each, given preferences
+# Make matrix with student preferences, one col per student,
+# one row per class
+s.prefs <- matrix(c(1,NA, 1,2, 1,NA, 2,1, 1,2, 1,2, 1,2), 2, 7)
+# Make matrix with professor preferences, one col per professor,
+# one row per student
+c.prefs <- matrix(c(1,2,3,4,5,6,7, 1,2,3,4,5,NA,NA), 7, 2)
+m2 <- hri(s.prefs=s.prefs, c.prefs=c.prefs, nSlots=c(3,3))
