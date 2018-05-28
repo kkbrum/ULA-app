1fe7a2fe7db650df4803dd3d82f9e39e5a751636
kkbrum <katherine.brumberg@yale.edu>
2018-05-28 11:10:12 -0400

    Turned log in to separate page

--- Student.R
+++ Student.R
@@ -24,63 +24,99 @@ ui <- fluidPage(
            }
            "
       )
-      )
-      ),
+    )
+  ),
   
   titlePanel("ULA application for S&DS courses"), 
   
-  mainPanel(width=12,
-            tabsetPanel(type = "tabs",
-                        # Collect all student meta data
-                        tabPanel("My Info", 
-                                 br(),
-                                 textInput("netid", "NetID (New Users Only)*", "", width="60%"),
-                                 textInput("new_pin", "Create a 4-digit pin for future login*", "", width="60%"),
-                                 textInput("first_name", "First Name*", "", width="60%"),
-                                 textInput("last_name", "Last Name*", "", width="60%"),
-                                 selectInput("year", "Class Year*", 
-                                             c("Select a year", 2018, 2019, 2020, 2021), width="60%"),
-                                 textInput("major", "Major*", "", width="60%"),
-                                 textAreaInput("why", 
-                                               "Why do you want to serve as a ULA?*", 
-                                               "", width="60%", height="60%"),
-                                 br(),
-                                 "(*) Denotes a required field."
-                        ),
-                        # Collect student prefererences
-                        tabPanel("Course Preferences", 
-                                 br(),
-                                 tableOutput("t_course"),
-                                 hr(),
-                                 fluidRow(
+  shinyjs::hidden(
+    mainPanel(id="main", width=12,
+              tabsetPanel(type = "tabs",
+                          # Collect all student meta data
+                          tabPanel("My Info", 
                                    br(),
-                                   DT::dataTableOutput("rankDT"),
+                                   textInput("netid", "NetID (New Users Only)*", "", width="60%"),
+                                   textInput("new_pin", "Create a 4-digit pin for future login*", "", width="60%"),
+                                   textInput("first_name", "First Name*", "", width="60%"),
+                                   textInput("last_name", "Last Name*", "", width="60%"),
+                                   selectInput("year", "Class Year*", 
+                                               c("Select a year", 2018, 2019, 2020, 2021), width="60%"),
+                                   textInput("major", "Major*", "", width="60%"),
+                                   textAreaInput("why", 
+                                                 "Why do you want to serve as a ULA?*", 
+                                                 "", width="60%", height="60%"),
                                    br(),
-                                   verbatimTextOutput('printForm')
-                                 )
-                        ),
-                        tabPanel("Summary", 
-                                 br(), 
-                                 htmlOutput("summarytext"), 
-                                 htmlOutput("summarytext2"), 
-                                 htmlOutput("ranked"),
-                                 br(), 
-                                 actionButton("submit", "Submit")
-                        )
-            )
+                                   "(*) Denotes a required field."
+                          ),
+                          # Collect student prefererences
+                          tabPanel("Course Preferences", 
+                                   br(),
+                                   tableOutput("t_course"),
+                                   hr(),
+                                   fluidRow(
+                                     br(),
+                                     DT::dataTableOutput("rankDT")
+                                   )
+                          ),
+                          tabPanel("Summary", 
+                                   br(), 
+                                   htmlOutput("summarytext"), 
+                                   htmlOutput("summarytext2"), 
+                                   htmlOutput("ranked"),
+                                   br(), 
+                                   actionButton("submit", "Submit")
+                          )
+                          
+              )
+              
+              
+    )
   ),
   
-  # Log in box
-  absolutePanel(id="loginbox", top="15%", right="8%", width="20%", draggable=TRUE,
-                textInput("username", "Username", "", width="90%"),
-                textInput("pin", "4-digit Pin", "", width="90%"),
-                actionButton("login", "Log in")
+  #Log in box
+  mainPanel(id="startPage", 
+            fluidRow(
+              column(6, 
+                     "To start a new application, press 'Begin'",
+                     br(),
+                     br(),
+                     actionButton("begin", "Begin")
+              ),
+              column(6,
+                     "To edit an existing application, log in:",
+                     br(),
+                     br(),
+                     div(id="loginbox", 
+                         textInput("username", "Username", "", width="90%"),
+                         textInput("pin", "4-digit Pin", "", width="90%"),
+                         actionButton("login", "Log in")
+                     )
+              )
+            )
   )
-      )
+  
+)
 
 
 server <- function(session, input, output) {
+  rv <- reactiveValues(page = 1, loginSuccess=FALSE)
   
+  observe({
+    if(rv$page==2) {
+      hide("startPage")
+      show("main")
+    }
+    
+  })
+  
+  navPage <- function(direction) {
+    rv$page <- rv$page + direction
+  }
+  
+  observeEvent(rv$loginSuccess, 
+               if(rv$loginSuccess == TRUE) {navPage(1)}
+  )
+  observeEvent(input$begin, navPage(1))
   
   # create a character vector of shiny inputs 
   shinyInput = function(FUN, len, id, ...) { 
@@ -123,7 +159,7 @@ server <- function(session, input, output) {
                              step = 1, width="70%")
   DF$Professor  <- shinyInput(textInput, nrow(DF), 'Professor', width="99%")
   DF$Grade  <- shinyInput(selectInput, nrow(DF), "Grade",
-                          choices = c("", "A", "B", "C", "D", "F"),
+                          choices = c("", "A", "A-", "B+", "B", "B-", "C+", "C, C-, D+", "D, D-, F", "CR, P"),
                           multiple = FALSE, selectize=FALSE, width="60%")
   DF$Suitable <- shinyInput(textInput, nrow(DF), "Suitable")
   DF$Rank <- shinyInput(numericInput, nrow(DF), 'Rank',
@@ -135,7 +171,7 @@ server <- function(session, input, output) {
                   "Who was your professor?",
                   "What was your Grade in this course?",
                   "Why are you Suitable for this course?",
-                  "Rank your preference of ULAing this course")
+                  "Rank your preference of ULAing this course (1 is the most preferred)")
   
   
   # Error checking
@@ -161,16 +197,15 @@ server <- function(session, input, output) {
   output$rankDT = DT::renderDataTable( 
     DF, server = FALSE, escape = 2, selection='none', options = list( 
       preDrawCallback = JS('function() { 
-                           Shiny.unbindAll(this.api().table().node()); }'), 
+                             Shiny.unbindAll(this.api().table().node()); }'), 
       drawCallback = JS('function() { 
-                        Shiny.bindAll(this.api().table().node()); } '),
+                          Shiny.bindAll(this.api().table().node()); } '),
       dom = 't', rownames = FALSE
-      ) 
-    )
+    ) 
+  )
   
   # Disabling/Enabling buttons      
   observeEvent(shinyValue("Taken",nrow(DF)), {
-    print(shinyValue("Taken", nrow(DF)))
     lapply(which(shinyValue("Taken", nrow(DF)) =="" | is.na(shinyValue("Taken", nrow(DF)))), function(j) {
       shinyjs::disable(paste0("WhenTaken",j))
       shinyjs::disable(paste0("Professor",j))
@@ -222,9 +257,9 @@ server <- function(session, input, output) {
                               Rank = shinyValue('Rank', nrow(DF))[ind])
     # Basic error checking of preferences
     input.correct <- rep(NA, nrow(preferences))
-    if (length(unique(preferences$Rank)) != nrow(preferences)) {
+    if (any(sort(preferences$Rank) != 1:nrow(preferences))) {
       input.correct <- rep(FALSE, nrow(preferences))
-      showNotification("Please pick unique ranks", duration=5, type="error")
+      showNotification(paste0("Please pick unique ranks from 1 to ", nrow(preferences), " (the number of classes you chose)."), duration=5, type="error")
     }
     for (i in 1:nrow(preferences)) {
       if (preferences$Taken[i] == "") {
@@ -248,7 +283,6 @@ server <- function(session, input, output) {
       }
     }
     if (all(input.correct)) {
-      showNotification("Courses correctly selected", duration=5, type="message")
       write.csv(preferences, paste0(input$netid, "_preferences.csv"), row.names = FALSE)
     } else {
       showNotification("Incorrect inputs", duration=5, type="error")
@@ -256,13 +290,14 @@ server <- function(session, input, output) {
     
   })
   
-  observeEvent(shinyValue("Rank",nrow(DF)),
-               if (sum(is.na(shinyValue("Rank",nrow(DF))))==0) {
+  observeEvent(c(shinyValue("Rank",nrow(DF)), rv$loginSuccess),
+               if (sum(is.na(shinyValue("Rank",nrow(DF)))) < nrow(DF)) {
                  hide("summarytext2")
-                 if (length(unique(shinyValue("Rank",nrow(DF)))) != length(shinyValue("Rank",nrow(DF)))) {
-                   output$ranked <- renderUI(HTML("<font color='red'>Please select unique rankings of courses.</font>"))
+                 ind <- which(shinyValue("Desire", nrow(DF))=="Y")
+                 if (any(sort(shinyValue("Rank",nrow(DF))[ind]) != 1:length(ind))) {
+                   output$ranked <- renderUI(HTML(paste0("<font color='red'>Please select unique ranks of courses from 1 to ", length(ind), " (the number of classes you chose).</font>")))
                  } else {
-                   output$ranked <- renderUI(HTML(paste0("<strong>You have selected (from highest to lowest preference): </strong> </br>", paste(input$choices[order(shinyValue('Rank', nrow(DF)))], collapse=", "))))
+                   output$ranked <- renderUI(HTML(paste0("<strong>You have selected (from highest to lowest preference): </strong> </br>", paste(DF[ind,'Course Title'][order(shinyValue('Rank', nrow(DF))[ind])], collapse=", "))))
                  }
                }
   )
@@ -279,6 +314,7 @@ server <- function(session, input, output) {
       updateSelectInput(session, 'year', selected = mydata$year)
       updateTextInput(session, 'major', value = mydata$major)
       updateTextAreaInput(session, 'why', value = mydata$why)
+      rv$loginSuccess <- TRUE
     }, error= function(e) {showNotification('User and pin not found', duration=5, type="error")})
     try({
       mypref <- read.csv(paste0(input$username, "_preferences.csv"), header=TRUE, as.is=TRUE)
@@ -301,7 +337,7 @@ server <- function(session, input, output) {
       DF[chosen,5] <- unlist(lapply(1:length(chosen), function(x) as.character(textInput(paste0('Professor', chosen[x]), width="99%", label=NULL,
                                                                                          value=mypref$Professor[row[x]]))))
       DF[chosen,6] <- unlist(lapply(1:length(chosen), function(x) as.character(selectInput(paste0("Grade", chosen[x]),
-                                                                                           choices = c("", "A", "B", "C", "D", "F"),
+                                                                                           choices = c("", "A", "A-", "B+", "B", "B-", "C+", "C, C-, D+", "D, D-, F", "CR, P"),
                                                                                            multiple = FALSE, selectize=FALSE, width="60%",
                                                                                            selected=mypref$Grade[row[x]], label=NULL))))
       DF[chosen,7] <- unlist(lapply(1:length(chosen), function(x) as.character(textInput(paste0("Suitable", chosen[x]), label=NULL, value=mypref$Suitable[row[x]]))))
@@ -315,10 +351,10 @@ server <- function(session, input, output) {
           drawCallback = JS('function() { 
                             Shiny.bindAll(this.api().table().node()); } '),
           dom = 't', rownames = FALSE
-          ) 
-        )
+        ) 
+      )
       
-          })
+    })
   })
   
   # Summary tab
