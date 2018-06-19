01778870648544ca1e6d2836d020fde135d2fb01
Maria Gargiulo <mariagargiulo.017@gmail.com>
2018-06-18 21:47:25 -0700

    working mini example of dragging and dropping in Shiny

--- Admin.R
+++ Admin.R
@@ -2,25 +2,27 @@ library(shiny)
 # devtools::install_github('ayayron/shinydnd')
 library(shinyDND)
  
-# Define UI for application that draws a histogram
 ui <- shinyUI(
   mainPanel(
     h2("DragUI"),
     dragUI("div6","bar"),
-    dragUI("div5","foo", style = "background-color:red", class = "dragelement"),
-    dragUI("div4",tags$a("a",href = "foo")),
+    dragUI("div5","foo"),
+    dragUI("div4", "hello"),
     h2("Drop UI"),
-    dropUI("div3",vertical = TRUE, nrow = 4),
-    dropUI("div2"),
-    h2("DragSetUI"),
-    dragSetUI("div1", textval = list("foo",tags$a("a",href = "bar"),"baz"))
+    h3("S&DS 361"),
+    dropUI("div3"),
+    h3("S&DS 361"),
+    dropUI("div2", "woo"), 
+    dropUI("div1")
   )
 )
 
 # server with reactive for observing reactive drop event
-server = shinyServer(function(input, output,session) {
+server <- shinyServer(function(input, output,session) {
   observeEvent(input$div2,{
     output$foo = renderText(
       paste("The dropUI element currently contains:", input$div2))
   })
 })
+
+shinyApp(ui, server)
