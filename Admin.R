630e20967af3976a6933ca801b3657bed0f2a235
kkbrum <katherine.brumberg@yale.edu>
2018-06-21 21:24:59 -0400

    You can now move students!

--- Admin.R
+++ Admin.R
@@ -5,26 +5,26 @@ library(shinyBS)
 
 # ======================= BEGIN MARIA'S FAILED ATTEMPTS ========================
 
-# Load this object in (generated from Matching.R)
-student_preferences <- readRDS("student_preferences.RDS")
-
-# Function that theoretically creates a pop-up with student preferences
-hover <- function(student) {
-  stu <- student_preferences$temp
-  bsPopover(id=input$student, 
-            title=student, 
-            content=(paste0(stu[, "Title"], ": ", stu[, "Rank"], collapse=" | ")),
-            placement="left")
-}
-
-# These names need to exist in the preferences df in order to get printout
-student_preferences$"Maria" <- student_preferences$"Maria G"
-student_preferences$"Katherine" <- student_preferences$"Katherine B"
-student_preferences$"Shah" <- student_preferences$"Student Five"
-
-# This doesn't work... at all, but I tried this in server. Needs a reactive
-# component and I don't think that buttons have fine enough id's currently
-lapply(students, hover)
+# # Load this object in (generated from Matching.R)
+# student_preferences <- readRDS("student_preferences.RDS")
+# 
+# # Function that theoretically creates a pop-up with student preferences
+# hover <- function(student) {
+#   stu <- student_preferences$temp
+#   bsPopover(id=input$student, 
+#             title=student, 
+#             content=(paste0(stu[, "Title"], ": ", stu[, "Rank"], collapse=" | ")),
+#             placement="left")
+# }
+# 
+# # These names need to exist in the preferences df in order to get printout
+# student_preferences$"Maria" <- student_preferences$"Maria G"
+# student_preferences$"Katherine" <- student_preferences$"Katherine B"
+# student_preferences$"Shah" <- student_preferences$"Student Five"
+# 
+# # This doesn't work... at all, but I tried this in server. Needs a reactive
+# # component and I don't think that buttons have fine enough id's currently
+# lapply(students, hover)
 
 # ======================= END MARIA'S FAILED ATTEMPTS ==========================
 
@@ -77,7 +77,7 @@ ui <- shinyUI(
 # server with reactive for observing reactive drop event
 server <- shinyServer(function(input, output,session) {
   
-  clicked <- reactiveValues(s = s)
+  clicked <- reactiveValues(s = s, change = FALSE)
   
   # Record whether students have been clicked
   lapply(students, function(x)
@@ -110,7 +110,7 @@ server <- shinyServer(function(input, output,session) {
   )
   
   # Render the lists of students in each course
-  observeEvent(course_assignments, {
+  observeEvent(clicked$change, {
     lapply(courses[-length(courses)], function(x)
       output[[paste0(x, "_list")]] <- renderUI({
         HTML(paste0(course_assignments[[x]], "</br>"))
@@ -130,10 +130,25 @@ server <- shinyServer(function(input, output,session) {
   
   # Move people around when course titles get clicked
   
+  remove_students <- function(students) {
+    for (student in students) {
+      for(course in courses) {
+        if(student %in% course_assignments[[course]]) {
+          course_assignments[[course]] <<- course_assignments[[course]][course_assignments[[course]] != student]
+        }
+      }
+    }
+  }
+  
   lapply(courses, function(x) {
     observeEvent(input[[x]], {
-      course_assignments[[x]] <- c(course_assignments[[x]], students[which(unlist(clicked$s))])
-      print(course_assignments[[x]])
+      changed_students <- students[which(unlist(clicked$s))]
+      remove_students(changed_students)
+      course_assignments[[x]] <<- c(course_assignments[[x]], changed_students)
+      for (student in changed_students) {
+        clicked$s[[student]] <- FALSE
+      }
+      clicked$change <- !clicked$change
     })
   })
 })
